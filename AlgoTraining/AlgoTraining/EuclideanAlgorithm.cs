90973a5e1a65b9a6927dd07947d414344dcb1d26
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2023-08-13 17:59:00 +0200

    docs: describe the purpose of `GetGreatestCommonDivisor` function

--- AlgoTraining/AlgoTraining/EuclideanAlgorithm.cs
+++ AlgoTraining/AlgoTraining/EuclideanAlgorithm.cs
@@ -2,6 +2,13 @@
 {
     public class EuclideanAlgorithm
     {
+        /// <summary>
+        /// Calculates the greatest common divisor of two numbers
+        /// using the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>GCD</returns>
         public static int GetGreatestCommonDivisor(int a, int b)
         {
             while(true)
